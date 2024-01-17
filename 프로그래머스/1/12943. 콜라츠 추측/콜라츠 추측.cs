9b9d1e565cd58c4b7a1f26b9ed188f7aff1aac3a
KDHwan <70990549+Hwan20173059@users.noreply.github.com>
2024-01-17 09:37:29 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.18 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long number = num;
+
+        while(number > 1 && answer < 500)
+        {
+           if (number % 2 == 0)
+             number /= 2;
+           else
+             number = (number * 3) + 1;
+    
+           answer++;
+        }
+
+        if (answer >= 500)
+           answer = -1;
+
+        return answer;
+    }
+}
