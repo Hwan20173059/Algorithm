9792f7404241acaef186a35b39ab1de7517cf209
KDHwan <70990549+Hwan20173059@users.noreply.github.com>
2024-03-14 09:27:13 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.29 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
@@ -1,35 +1,29 @@
 using System;
 
 public class Solution {
-    public int[] solution(string[] wallpaper) {
+    public int[] solution(string[] wallpaper){
         int[] answer = new int[4];
 
         answer[0] = wallpaper.Length;
         answer[1] = wallpaper[0].Length;
-
-        int curX;
-        int curY;
 
         for(int i = 0; i < wallpaper.Length; i++)
         {
             for(int j = 0; j < wallpaper[i].Length; j++)
             {
                 if (wallpaper[i][j] == '#')
-                {
-                    curX = i;
-                    curY = j;
-                    
-                    if (answer[0] > curX)
-                        answer[0] = curX;
+                {                     
+                    if (answer[0] > i)
+                        answer[0] = i;
 
-                    if (answer[1] > curY)
-                        answer[1] = curY;
+                    if (answer[1] > j)
+                        answer[1] = j;
                     
-                    if (answer[2] < curX + 1)
-                        answer[2] = curX + 1;
+                    if (answer[2] < i + 1)
+                        answer[2] = i + 1;
 
-                    if (answer[3] < curY + 1)
-                        answer[3] = curY + 1;
+                    if (answer[3] < j + 1)
+                        answer[3] = j + 1;
                 }
             }
         }
