595bc8bcb2fdfff802d73030bfe0eab2ce9efa03
KDHwan <70990549+Hwan20173059@users.noreply.github.com>
2024-04-12 09:31:02 +0900

    [level 2] Title: 의상, Time: 2.20 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(string[,] clothes) {
+        int answer = 1;
+        Dictionary<string, int> clothesCnt = new Dictionary<string, int>();
+
+        for(int i = 0; i < clothes.GetLength(0); i++)
+        {
+            string type = clothes[i, 1];
+
+            if (clothesCnt.ContainsKey(type))
+                clothesCnt[type]++;
+            else
+                clothesCnt[type] = 1;
+        }
+
+        foreach (var count in clothesCnt.Values)
+            answer *= (count + 1);
+
+        answer--;
+
+        return answer;
+    }
+}
